d0bba7d28feed8646be2aab0100ce6d66ce7d11b
pommuraj -17uite054 <pommuraj2000@gmail.com>
2022-09-20 23:44:46 +0530

    css changes

--- js/script.js
+++ js/script.js
@@ -25,13 +25,11 @@ $(document).ready(function () {
   //select the POPUP FRAME and show it
   $("#popup").hide().fadeIn(1000);
   $("#overlay").hide().fadeIn(1000);
-  $("#body").css("overflowY", "hidden");
   //close the POPUP if the button with id="close" is clicked
   $("#close").on("click", function (e) {
     e.preventDefault();
     $("#popup").fadeOut(1000);
     $("#overlay").fadeOut(1000);
-    $("#body").css("overflowY", "auto");
   });
 });
 
