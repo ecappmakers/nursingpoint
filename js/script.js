547b0a4da925568c76be2dedd9f9443c090b297f
POMMURAJ <pommuraj@POMMURAJs-MacBook-Air.local>
2026-07-05 22:11:27 +0530

    nursing point 2

--- js/script.js
+++ js/script.js
@@ -22,6 +22,22 @@ function submitAppointmentForm() {
 //////////////////////// ad slider js /////////////////////////////
 //with this first line we're saying: "when the page loads (document is ready) run the following script"
 $(document).ready(function () {
+  // Fix navbar active states dynamically based on current path
+  var currentPath = window.location.pathname;
+  $('.navbar-nav .nav-link').removeClass('active');
+  
+  $('.navbar-nav .nav-link').each(function () {
+    var href = $(this).attr('href');
+    if (href === currentPath || (href !== '/' && currentPath.indexOf(href) === 0)) {
+      $(this).addClass('active');
+    }
+  });
+  
+  // Root URL fallback
+  if (currentPath === '/' || currentPath === '/index.php' || currentPath === '/index-chennai.php' || currentPath === '') {
+    $('.navbar-nav .nav-link[href="/"]').addClass('active');
+  }
+
   //select the POPUP FRAME and show it
   $("#popup").hide().fadeIn(1000);
   $("#overlay").hide().fadeIn(1000);
